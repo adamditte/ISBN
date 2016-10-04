113206699c928e501984665843712cae23aa04e5
Adam Dittebrand <Adam Dittebrand>
2016-10-04 13:51:30 -0400

    mob coding the isbn checker

---
+++ ISBN/isbnmob.rb
@@ -0,0 +1,8 @@
+def valid_isbn?(isbn)
+	if isbn.length == 10
+		true
+	else
+		false
+	end
+
+end
---
+++ ISBN/isbnmobtest.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require_relative "isbnmob.rb"
+
+class TestISBN < Minitest::Test
+	def test_valid_ten_digit_isbn_returns_true
+		isbn = "0471958697"
+		assert_equal(true, valid_isbn?(isbn))
+	end
+
+	def test_empty_string_returns_false
+		isbn = ""
+		assert_equal(false, valid_isbn?(isbn))
+	end
+
+end
+
