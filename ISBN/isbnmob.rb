6a9b912e2acb55d32109971346e4f8881de3ab3b
Adam Dittebrand <Adam Dittebrand>
2016-10-04 14:51:06 -0400

    adding testing for letters, dashes, and spaces.

--- ISBN/isbnmob.rb
+++ ISBN/isbnmob.rb
@@ -1,6 +1,18 @@
 def valid_isbn?(isbn)
-	isbn.gsub!(" ", "")
-	isbn.gsub!("-", "") #this strips the whitespace and redefines isbn using the !
+	isbn = removing_spaces(isbn)
+	isbn = removing_dashes(isbn)
+
 	isbn.length == 10 #same as doing if else statement
 
+	#isbn.delete(" ") is an alternative to gsub
+
+end
+
+def removing_spaces(isbn)
+	isbn.gsub(" ", "") #this strips the space and redefines isbn using the !
+end
+
+def removing_dashes(isbn)
+	isbn.gsub("-", "") #this strips the dashes and redefines isbn using the !
+	
 end
