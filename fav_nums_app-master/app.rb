d4629bcc316b27dc382703baa6e1b592d418f7a1
Adam Dittebrand <Adam Dittebrand>
2016-10-09 15:20:06 -0400

    starting to add javascript

--- fav_nums_app-master/app.rb
+++ fav_nums_app-master/app.rb
@@ -68,7 +68,17 @@ post '/change' do
 	erb :change_returned, :locals => {:name => name, :stuff => stuff, :input_amount_to_be_reduced => input_amount_to_be_reduced }
 end
 
+get '/contact' do 
+	erb :contact
+end
 
+get '/about' do
+	erb :about
+end
+
+get '/fun' do
+	erb :fun
+end
 
 
 
