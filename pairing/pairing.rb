e14dd492bd3eb806ad648f4cec1cbfae2e69d489
Adam Dittebrand <Adam Dittebrand>
2016-10-11 09:18:50 -0400

    using a different approach. Works better,

--- pairing/pairing.rb
+++ pairing/pairing.rb
@@ -1,3 +1,38 @@
-def name_array(array)
-	array.shuffle.each_slice(2).to_a
+# def name_array(array)
+#    group_size = 2
+#    # number_of_elements = array.length
+#    # number_of_arrays = number_of_elements / group_size.to_i
+#    puts "array = #{array}"
+#    puts "array.class = #{array.class}"
+#    shuffled = array.shuffle
+#    sliced = shuffled.each_slice(group_size.to_i).to_a
+#    if shuffled.length % 2 == 1
+#         sliced[-2] << sliced[-1][0]
+#         sliced.delete_at(-1)
+#     else
+#         sliced
+#    end
+#    sliced
+# end
+
+def randomizer(names_array) # name of my function and variable
+
+    shuffled = names_array.shuffle # sets a shuffled names_array equal to shuffled
+
+    sliced = shuffled.each_slice(2).to_a # go through each element in shuffled and set them in arrays of two
+
+    if shuffled.length % 2 == 1 # looks to see if the length of shuffled divided by 2 has a remainder of 1
+
+        sliced[-2] << sliced[-1][0] # takes the element in the last array of the sliced array and puts it in the second to last sliced array
+
+        sliced.pop # cuts the last element off the sliced array
+
+    else
+
+        sliced
+
+    end
+
+    sliced
+
 end
--- pairing/pairingtest.rb
+++ pairing/pairingtest.rb
@@ -8,21 +8,21 @@ class TestPairing < Minitest::Test
 
 	def test_for_empty_array
 		array = []
-		assert_equal([], name_array(array))
+		assert_equal([], randomizer([]))
 	end
 
 	def test_for_four_names
-		array = name_array(["susan", "joe", "cyndi", "marv"])
+		array = randomizer(["susan", "joe", "cyndi", "marv"])
 		assert_equal(2, array.count)
 	end
 
 	def test_for_pairing_six_names
-		array = name_array(["susan", "joe", "cyndi", "marv","jeremy","don"])
+		array = randomizer(["susan", "joe", "cyndi", "marv","jeremy","don"])
 		assert_equal(3, array.count)
 	end
 
 	def test_al_names_returns_6
-		array = name_array(["susan", "joe", "cyndi", "marv","jeremy","don", "stepahnie","susan","adam","mike","maxx","john"])
+		array = randomizer(["susan", "joe", "cyndi", "marv","jeremy","don", "stepahnie","susan","adam","mike","maxx","john"])
 		assert_equal(6, array.count)
 	end
 	
