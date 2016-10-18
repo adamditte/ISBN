8027e7ed9ef77b1b0e79a83df3bc9201fccf882f
Adam Dittebrand <Adam Dittebrand>
2016-10-18 13:30:29 -0400

    tests for names and sounds of animals.

---
+++ animal_sounds/fox.rb
@@ -0,0 +1,8 @@
+class Fox
+	attr_reader :name, :sound
+
+	def initialize(name)
+		@name = name
+		@sound = "meep"
+	end
+end
---
+++ animal_sounds/test_animal_sounds.rb
@@ -0,0 +1,38 @@
+require "minitest/autorun"
+require_relative "cat.rb"
+require_relative "dog.rb"
+require_relative "fox.rb"
+
+class TestAnimalSounds < Minitest::Test
+
+	def test_cat_name
+		animal = Cat.new("Kitty")
+		assert_equal("Kitty", animal.name)
+	end 
+
+	def test_fox_name
+		animal = Fox.new("Mister")
+		assert_equal("Mister", animal.name)
+	end 
+	def test_dog_name
+		animal = Dog.new("Frank")
+		assert_equal("Frank", animal.name)
+	end 
+
+	def test_cat_sound
+		animal = Cat.new("Kitty")
+		assert_equal("meow", animal.sound)
+	end
+
+	def test_fox_sound
+		animal = Fox.new("Mister")
+		assert_equal("meep", animal.sound)
+	end
+
+	def test_dog_sound
+		animal = Dog.new("Frank")
+		assert_equal("woof", animal.sound)
+	end
+
+
+	end
