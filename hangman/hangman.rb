393fd914fa2271c1f2ff4dfc4a6bc0f94f0adb89
Adam Dittebrand <Adam Dittebrand>
2016-11-29 13:50:04 -0500

    first game test passes

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -5,9 +5,12 @@ class Hangman
     def initialize(word)
 
 	    (@word = word).downcase!
-		#(@word = word)
 		@spaces = "_" * word.length
 		@bad_guesses = 10
 		@used_letters = []
+    end
+
+    def used_letters?(choice)
+		@already_guessed.include? choice 
     end
 end
--- hangman/hangman_test.rb
+++ hangman/hangman_test.rb
@@ -9,3 +9,4 @@ class TestHangman < Minitest::Test
 		assert_equal("beer", game.word)
     end
 end
+    def test_to_see_if_used_letter_gets_used
